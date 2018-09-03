99f013b6d6af7db9570b6f292134a66b71157a79
nate-river <l.luffy.river@gmail.com>
2018-09-03 23:21:40 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,82 +21,140 @@ Page({
       }
     })
   },
+  formSubmit(e) {
+    this.setData({
+      form: e.detail.value
+    })
+    this.setData({
+      'form.user_name': this.data.user_info.nickName,
+      'form.user_img': this.data.user_info.avatarUrl
+    })
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          //发起网络请求
+          this.setData({
+            "form.jscode": res.code
+          })
+          wx.request({
+            url: 'https://event.applinzi.com/index.php?type=add', //仅为示例，并非真实的接口地址
+            method: "POST",
+            data: this.data.form,
+            header: {
+              'content-type': 'application/x-www-form-urlencoded' // 默认值
+            },
+            success: function (res) {
+              console.log(res.data)
+              // 拿到活动Id
+              // 吊起弹出窗口
+              
+            }
+          })
+
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
+
+  },
+  // 点击分享到好友 调微信接口
+  // 点击分享到朋友圈   下载图片 把图片在页面中显示
+  // 点击保存到相册   调微信接口
 
   /**
    * 页面的初始数据
    */
   data: {
-    status: true　　　　　　　　　　　　//data里面的属性可以传递到视图
+    user_info: null,
+    status: true,　　　　　　　　　　　//data里面的属性可以传递到视图
+    form: {
+      title: Math.random()
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  wx.request({
-      url: 'https://event.applinzi.com/index.php?type=add', //仅为示例，并非真实的接口地址
-      method:"post",
-      data: {
-        x: '',
-        y: ''
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)
-      }
-    })
+    // wx.request({
+    //   url: 'https://event.applinzi.com/index.php?type=add', //仅为示例，并非真实的接口地址
+    //   method: "post",
+    //   data: {
+    //     x: '',
+    //     y: ''
+    //   },
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data)
+    //   }
+    // })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    if (!this.data.user_info) {
+      if (wx.getStorageSync('user_info')) {
+        this.setData({
+          user_info: wx.getStorageSync('user_info')
+        })
+      } else {
+
+      }
+    }
+  },
+  get(e) {
+    this.setData({
+      user_info: e.detail.userInfo
+    })
+    wx.setStorageSync('user_info', e.detail.userInfo)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
- 
+
 
 })
--- pages/me/me.js
+++ pages/me/me.js
@@ -6,37 +6,29 @@ Page({
    */
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    islogin:false,
-    list:[],
-    jscode:'1234'
+    islogin: false,
+    list: [],
+    jscode: '1234'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var value = wx.getStorageSync('key')
-    if(value){
-       this.setData({
-         islogin:true
-       })
-       this.getData();
-    }else{
-       this.login();
-    }
-  },
-  login(){
 
   },
-  getData(){
+  login() {
+
+  },
+  getData() {
     wx.request({
       url: 'https://event.applinzi.com/index.php?type=my',
-      data:{
-        jscode:this.data.jscode
+      data: {
+        jscode: this.data.jscode
       },
-      success(res){
+      success(res) {
         this.setData({
-          list:res.data
+          list: res.data
         })
       }
     })
@@ -45,54 +37,70 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
   bindGetUserInfo: function (e) {
-    if(this.data.islogin){
+    if (this.data.islogin) {
       console.log(e.detail.userInfo.avatarUrl)
-    }else{
+    } else {
 
     }
   },
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  // user_info
+  // 没有user_infp
+  // wx.navigateback
   
+  // 进来的人一定有user_info
+  // img name
+  // js_code  请求你的数据
+
+  onShow: function () {
+    var value = wx.getStorageSync('key')
+    if (value) {
+      this.setData({
+        islogin: true
+      })
+      this.getData();
+    } else {
+      this.login();
+    }
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
